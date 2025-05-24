ce3813a9d30dd4fbbd6a2871925de4adfb80d000
dhanya017 <dhanyagireesan017@gmail.com>
2025-05-25 02:20:59 +0530

    APIs for company-contact-map

---
+++ DTOs/ContactCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace XLead_Server.DTOs
+{
+    public class ContactCreateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public long CompanyId { get; set; }
+        public long CreatedBy { get; set; }
+    }
+}
---
+++ Interfaces/ICompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using XLead_Server.DTOs;
+using XLead_Server.Models;
+
+namespace XLead_Server.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        Task<Company> AddCompanyAsync(CompanyCreateDto dto);
+        Task<Dictionary<string, List<string>>> GetCompanyContactMapAsync();
+        Task<IEnumerable<CompanyReadDto>> GetAllCompaniesAsync();
+    }
+}

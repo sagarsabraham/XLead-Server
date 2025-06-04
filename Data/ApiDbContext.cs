9b9e0c624c3709dbfd2986192f836f377621be41
Harinand S <harinand.s@experionglobal.local>
2025-06-04 19:27:41 +0530

    added contact

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -19,6 +19,8 @@ namespace XLead_Server.Data
         public DbSet<Country> Countries { get; set; }
         public DbSet<DealStage> DealStages { get; set; }
 
+        public DbSet<User> Users { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
